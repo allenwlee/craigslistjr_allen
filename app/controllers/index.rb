6b17c45823eab65601a0c901563c3bd7c44a5e26
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-05-04 08:28:15 -0700

    frontend MVP done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,32 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/categories' do
+  @categories = Category.all
+  erb :categories
+end
+
+get '/posts' do
+end
+
+get '/posts' do
+end
+
+get '/post/:id' do
+end
+
+get '/post/create' do
+end
+
+
+get '/post/:id/edit' do
+end
+
+
+get '/category/:id' do
+end
+
+get '_nav' do
+  erb :_nav
+end
